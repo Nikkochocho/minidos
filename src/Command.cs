2212b673f9717dbfb6fb6c41cabf4246d6a2b6a6
nikkochocho <nikkochocho@gmail.com>
2024-08-18 16:49:47 -0300

    FInished command shell commands and machine shutdown

---
+++ src/Command.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shell
+{
+    public class Command
+    {
+        private bool _shutdown = false;
+
+        public bool Shutdown { get { return _shutdown; } }
+
+        private bool GetOneParm(string[] parms, out string ret)
+        {
+            ret = default;
+
+            if ((parms.Length == 2) && (parms[1] != ""))
+            {
+                ret = parms[1];
+
+                return true;
+            }
+            return false;
+        }
+
+        private bool GetTwoParms(string[] parms, out string ret1, out string ret2)
+        {
+            ret1 = ret2 = default;
+ 
+            if ((parms.Length == 3) && (parms[1] != "") && (parms[2] != ""))
+            {
+                ret1 = parms[1];
+                ret2 = parms[2];
+
+                return true;
+            }
+            return false;
+        }
+
+
+        public bool Exec(string cmd) 
+        {
+            var parms = cmd.Split(' ');
+
+            if (parms.Length > 0)
+            {
+                switch (parms[0].ToLower())
+                {
+                    case "dir":
+                        {
+                            if(parms.Length == 1)
+                            {
+                                return true;
+                            }
+                            return false;
+                        }
+
+                    case "cd":
+                        {
+                            if (GetOneParm(parms, out string ret))
+                            {
+                                Console.WriteLine("PARM CD " + ret);
+                                return true;
+                            }
+                            return false;
+                        }
+
+                    case "md":
+                        {
+                            if (GetOneParm(parms, out string ret))
+                            {
+                                Console.WriteLine("PARM MD " + ret);
+                                return true;
+                            }
+                            return false;
+                        }
+
+                    case "rd":
+                        {
+                            if (GetOneParm(parms, out string ret))
+                            {
+                                Console.WriteLine("PARM RD " + ret);
+                                return true;
+                            }
+                            return false;
+                        }
+
+                    case "del":
+                        {
+                            if (GetOneParm(parms, out string ret))
+                            {
+                                Console.WriteLine("PARM DEL " + ret);
+                                return true;
+                            }
+                            return false;
+                        }
+
+                    case "copy":
+                        {
+                            if (GetTwoParms(parms, out string ret1, out string ret2))
+                            {
+                                Console.WriteLine($"PARM COPY {ret1} {ret2}");
+                                return true;
+                            }
+                            return false;
+                        }
+
+                    case "ren":
+                        {
+                            if (GetTwoParms(parms, out string ret1, out string ret2))
+                            {
+                                Console.WriteLine($"PARM REN {ret1} {ret2}");
+                                return true;
+                            }
+                            return false;
+                        }
+
+                    case "shutdown":
+                        _shutdown = true;
+                        break;
+
+                    default:
+                        return (parms[0].Length == 0);
+                }
+            }
+            return false;
+        }
+    }
+}
--- src/Kernel.cs
+++ src/Kernel.cs
@@ -1,24 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using Sys = Cosmos.System;
+using Shell;
 
 namespace MiniDOS
 {
     public class Kernel : Sys.Kernel
     {
+        private static string __YEAR = "2024";
+        private static string __VERSION = "0.1";
+        private static string __AUTHOR = "Lara H. Ferreira";
+
+        private Command cmd = new Command();
 
         protected override void BeforeRun()
         {
-            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            string license = $"minidos v{__VERSION}. CopyLeft (c) {__YEAR} by {__AUTHOR}\n" +
+                             "This program comes with ABSOLUTELY NO WARRANTY;\n" + 
+                             "This is free software, and you are welcome to redistribute it under\n" +
+                             "certain conditions.\n\n";
+
+            Console.WriteLine(license);
         }
 
         protected override void Run()
         {
-            Console.Write("Input: ");
+            Console.Write("# ");
             var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+            if (!cmd.Exec(input))
+            {
+                Console.WriteLine("Incorrect command syntax");
+            }
+
+            if(cmd.Shutdown)
+            {
+                this.Stop();
+            }
         }
     }
 }
