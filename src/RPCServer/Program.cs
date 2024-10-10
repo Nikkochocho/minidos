ea0b2d2b96f6f128aadc0e4a665738f46d54e990
KHANSINGH\PopolonY2k <popolony2k@popolony2k.com.br>
2024-10-10 18:36:14 -0300

    Code cleaning
    Code cleaning

--- src/RPCServer/Program.cs
+++ src/RPCServer/Program.cs
@@ -7,44 +7,17 @@ namespace RPCServerApp
     {
         static void Main(string[] args)
         {
-            //if (args.Length == 0)
-            //{
-            //    Process process = new Process();
-            //    process.StartInfo.FileName = Process.GetCurrentProcess().MainModule.FileName;
-            //    process.StartInfo.UseShellExecute = false;
-            //    process.StartInfo.RedirectStandardOutput = true;
-            //    process.StartInfo.Arguments = "child";
-            //    //process.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
-            //    //{
-            //    //    // Prepend line numbers to each line of the output.
-            //    //    if (!String.IsNullOrEmpty(e.Data))
-            //    //    {
-            //    //        int lineCount = 0;
-            //    //        lineCount++;
-            //    //    }
-            //    //});
+            RPCLibrary.Server.RPCServer server = new RPCLibrary.Server.RPCServer(IPAddress.Any, 1999);
 
-            //    process.Start();
-
-            //    // Asynchronously read the standard output of the spawned process.
-            //    // This raises OutputDataReceived events for each line of output.
-            //    process.BeginOutputReadLine();
-            //    process.WaitForExit();
-            //}
-            //else
+            if (server.Start())
             {
-                RPCLibrary.Server.RPCServer server = new RPCLibrary.Server.RPCServer(IPAddress.Any, 1999);
-
-                if (server.Start())
-                {
-                    Console.WriteLine("RPC Server Started");
-                    server.Run();
-                    Console.WriteLine("RPC Server Finished");
-                }
-                else
-                {
-                    Console.WriteLine("Error to start RPC server");
-                }
+                Console.WriteLine("RPC Server Started");
+                server.Run();
+                Console.WriteLine("RPC Server Finished");
+            }
+            else
+            {
+                Console.WriteLine("Error to start RPC server");
             }
         }
     }
